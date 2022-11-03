db1831fbecfdd08b4e3457ef84ef4210eacfa328
yoonhocho <yoonhocho@localhost>
2022-11-03 12:47:45 -0500

    Rizz

--- src/studio7/HockeyPlayer.java
+++ src/studio7/HockeyPlayer.java
@@ -2,4 +2,21 @@ package studio7;
 
 public class HockeyPlayer {
 
+	
+	private String name;
+	private int number;
+	private int goals;
+	private int assists;
+	
+	
+	
+	public HockeyPlayer(String initName, int initNumber, int initGoals, int initAssists) {
+		name = initName;
+		number = initNumber;
+		goals = initGoals;
+		assists = initAssists;
+	
+	
+	}
+	
 }
